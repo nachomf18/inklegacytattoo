93beba2d5c371288820f081b47b68e01a82c35d1
Ignacio Martín <nachomfernandez@gmail.com>
2025-11-29 13:42:53 +0100

    Ajustes en formualrio de contacto

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,11 +6,29 @@ if (nombre) {
 
 form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
     const p = document.createElement("p");
-    p.textContent = "¡Gracias por contactarnos! Nos pondremos en contacto contigo pronto.";
+    p.textContent = "¡Gracias por escribirnos! Nos pondremos en contacto contigo pronto.";
     p.style.marginTop = "20px";
     p.style.fontSize = "1em";
 
-    form.appendChild(p);
     form.reset();
+    form.appendChild(p);
+});
+
+const emailInput = document.getElementById("email");
+emailInput.addEventListener("input", () =>  {
+    const placeholder = emailInput.nextElementSibling;
+    if (emailInput.value !== "") {
+        placeholder.style.transform = "translateY(-70px)";
+        placeholder.style.color = "var(--old-gold)";
+        placeholder.style.fontSize = "16px";
+        placeholder.style.transition = "all 0.3s ease-in-out";
+    } else {
+        placeholder.style.transform = "translateY(-45px)";
+        placeholder.style.fontSize = "18px";
+        placeholder.style.color = "rgba(13, 13, 13, 0.5)";
+        placeholder.style.transition = "all 0.3s ease-in-out";
+    }
 });
