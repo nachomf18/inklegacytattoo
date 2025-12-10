54a144ee01dc2b3fd9c9d5dc393adb9d4f97475f
Ignacio Martín Fernández <i.martinf@BUINF.local>
2025-12-10 09:48:00 +0100

    Control por teclado

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -34,6 +34,28 @@ btnDer.addEventListener('click', () => {
     update();
 });
 
+document.addEventListener('keydown', (e) => {
+    let visibleImages;
+    if (window.innerWidth > 1024) {
+        visibleImages = 3;
+    } else if (window.innerWidth > 767) {
+        visibleImages = 2;
+    } else {
+        visibleImages = 1;
+    }
+    const maxIndex = listaImg.length - visibleImages;
+
+    if (e.key === 'ArrowLeft' && index > 0) {
+        index -= 1;
+        update();
+        btnIzq.focus();
+    } else if (e.key === 'ArrowRight' && index < maxIndex) {
+        index += 1;
+        update();
+        btnDer.focus();
+    }
+});
+
 window.addEventListener('resize', () => {
     index = 0;
     update();
