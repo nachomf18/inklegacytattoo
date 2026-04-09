70483d52dad198595126800088de0e4f95e0c0fa
Ignacio Martín Fernández <i.martinf@BUINF.local>
2026-04-09 10:37:46 +0200

    Primer commit

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS "inklegacy";
+USE "inklegacy";
+
+CREATE TABLE tatuadores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    instagram VARCHAR(255) NOT NULL,
+    imagen VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tatuajes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ruta VARCHAR(255) NOT NULL,
+    id_tatuador INT NOT NULL,
+    FOREIGN KEY (id_tatuador) REFERENCES tatuadores(id)
+);
+
+CREATE TABLE mensajes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    asunto VARCHAR(255) NOT NULL,
+    mensaje TEXT NOT NULL,
+    id_tatuador INT NOT NULL,
+    FOREIGN KEY (id_tatuador) REFERENCES tatuadores(id)
+);
+
+CREATE TABLE citas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefono VARCHAR(255) NOT NULL,
+    fecha DATETIME NOT NULL,
+    id_tatuador INT NOT NULL,
+    FOREIGN KEY (id_tatuador) REFERENCES tatuadores(id)
+);
